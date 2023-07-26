557dc8d817adbdd1506b4fdf9c507bca31204c16
alpha-jung <shter7119@gmail.com>
2023-07-26 20:05:24 +0900

    Session Storage 에 Access Token 과 유효기간 값  체크하여 만료 1분 전에 Refresh Token 통해서 새로운 Access Token 발급

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './routes/login/LoginForm';
+import Register from './routes/login/Register';
+import Home from './routes/Home';
+import axios from 'axios';
+import OAuth2Login from './routes/login/OAuth2Login';
 
 function App() {
+  const at = sessionStorage.getItem("at");
+
+  useEffect(() => {
+    if(at != null) {
+      silentRefresh();
+    }
+  }, []);
+
+  function silentRefresh() {
+    axios
+      .post("http://localhost:9080/mongo/users/silentRefresh", {},
+      {
+        withCredentials: true
+      })
+      .then((res) => {
+        const newAt = res.headers.authorization;
+        const newExpireAt = res.headers.expireat;
+        refreshSuccess(newAt, newExpireAt);
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
+      });
+  }
+
+  function refreshSuccess(newAt: string, newExpireAt: string) {
+    sessionStorage.setItem('at', newAt);
+    sessionStorage.setItem('expireAt', newExpireAt);
+
+    let now = new Date();
+    let refreshTime = parseInt(newExpireAt) - now.getTime() - 60000;
+
+    setTimeout(silentRefresh, refreshTime);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {
+          !at ?
+          <Route path="/" element={ <LoginForm></LoginForm> } />
+          :
+          <Route path="/" element={ <Home></Home> } />
+        }
+        
+        <Route path="/register" element={ <Register></Register> } />
+        <Route path="/oauth2Login" element={ <OAuth2Login></OAuth2Login> } />
+      </Routes>
     </div>
   );
 }
